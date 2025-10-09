c9e073d2e8531a84fef9f8d375c36914834072db
Ctot2 <williamso27@gfacademy.org>
2025-10-08 20:51:50 -0400

    last day of work on the sudoku project

--- Data science projects/Sudoku/sudoku_game.py
+++ Data science projects/Sudoku/sudoku_game.py
@@ -2,40 +2,59 @@
 class Sudoku:
     game_board = []
     is_game_over = False
-    def __init__(self, game_board):
-        self.board(game_board)
+    def __init__(self):
+        self.game_board = self.create_board(game_board=[])
+        self.is_game_over = False
 
 
-    def board(self, game_board):
-        for i in range(9):
-                game_board.append(["-"]*9)
+    def create_board(self, game_board):
+        for i in range(1):
+                game_board.append([1, 2, 3, 4, 5, 6, 7, 8, "-"])
+                game_board.append([2, 3, 4, 5, 6, 7, 8, 9, "-"])
+                game_board.append([3, 4, 5, 6, 7, 8, 9, 1, "-"])
+                game_board.append([4, 5, 6, 7, 8, 9, 1, 2, "-"])
+                game_board.append([5, 6, 7, 8, 9, 1, 2, 3, "-"])
+                game_board.append([6, 7, 8, 9, 1, 2, 3, 4, "-"])
+                game_board.append([7, 8, 9, 1, 2, 3, 4, 5, "-"])
+                game_board.append([8, 9, 1, 2, 3, 4, 5, 6,"-"])
         return game_board
 
-    def print_board(self, game_board):
-        for i in range(len(game_board)):
-            print(" ".join([str(x) for x in game_board[i]]))
+
+    def print_board(self):
+        for row in self.game_board:
+            print(" ".join(map(str, row)))
 
 
-    def take_player_move(self, game_board, x, y, z, is_game_over):
+    def take_player_move(self):
             player_input = list(map(int, input("Enter three integers separated by spaces: ").split()))
-            x = player_input[0]
-            y = player_input[1]
-            z = player_input[2]
-
-            game_board[x][y] = z
-            return x, y, z
+            x, y, z = player_input
+            self.update_board(x, y, z)
+            #return x, y, z
 
 
             #self.update_board(game_board, x, y, z)
 
-    def update_board(self, game_board, x, y, z):
-            game_board[x][y] = z
+    def update_board(self, x, y, z):
+            self.game_board[x][y] = z
 
-    def game_over(self, game_board, is_game_over):
-        for i in range(9):
-                if 1 and 2 and 3 and 4 and 5 and 6 and 7 and 8 and 9 in game_board[i]:
-                    is_game_over = True
-                    self.take_player_move(game_board, is_game_over)
+    def game_over(self):
+        # Check if the game is over
+        vertical_list1 = []
+        for n in range (9):
+            for i in range(9):
+                    vertical_list1 = [row[n] for row in self.game_board]
+                    if sorted([cell for cell in vertical_list1 if cell != "-"]) != list(range(1, 10)):
+                        return False
+
+
+        for row in self.game_board:
+            if sorted([cell for cell in row if cell != "-"]) != list(range(1, 10)):
+                return False
+
+        print("you won!")
+        return True
+
+
 
 
 
