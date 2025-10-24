35c55fcd1f97ed569b17e7aa5f36466033998199
Ctot2 <williamso27@gfacademy.org>
2025-10-24 13:44:02 -0400

    queueueueueueueueueueueus

---
+++ Data science projects/Queues_project/Queues.py
@@ -0,0 +1,42 @@
+class Queue:
+
+    def __init__ (self):
+        self.items = []
+
+    def enqueue(self, input_value):
+        self.items.append(input_value)
+
+    def dequeue(self):
+        return self.items.pop(0)
+
+
+    def empty(self):
+        return True if self.items else False
+
+    def look(self):
+        return self.items[0]
+
+    def size(self):
+        return len(self.items)
+
+    def contains(self, input):
+        x = False
+        for i in range(self.size()):
+            temp = self.dequeue()
+            self.enqueue(temp)
+            if temp == input:
+                x = True
+        return x
+    
+    def __str__(self):
+        our_str = ""
+        for i in range(self.size()):
+            temp3 = self.dequeue()
+            self.enqueue(temp3)
+            our_str = our_str + temp3
+        return our_str
+
+    def __add__(self, other):
+        new_queue = Queue()
+        new_queue.items = self.items + other.items
+        return new_queue
