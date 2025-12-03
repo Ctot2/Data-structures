741e79fcd0a9e6634a8212cc3bac79d556cf0632
Ctot2 <williamso27@gfacademy.org>
2025-12-02 21:29:32 -0500

    now working fractal

--- Data science projects/fractals!/main.py
+++ Data science projects/fractals!/main.py
@@ -1,5 +1,6 @@
 from graphics import *
-
+win = GraphWin("My Fractal", 1000, 800)
+win.setBackground(color="white")
 
 
 from math import *
@@ -10,17 +11,17 @@ def find_point(p1, distance, angle):
    ycor = sin(rad_angle) * distance + p1.getY()
    return Point(xcor, ycor)
 
+
 def version1(d, dist, ang, x1, y1):
     if d == 0:
         return
     else:
+
         found_point = find_point(p1=Point(x1, y1), distance=dist, angle=ang)
         Line(Point(x1, y1), Point(found_point.getX(), found_point.getY())).draw(win)
-        version1(d-1, dist*0.8, ang-90, found_point.getX(), found_point.getY())
-        version1(d - 1, dist*0.8, ang+90, found_point.getX(), found_point.getY())
+        version1(d - 1, dist * 0.65, ang - 90, found_point.getX(), found_point.getY())
+        version1(d - 1, dist / 1.5, ang + 30, found_point.getX(), found_point.getY())
 
-    win.getMouse()  # Pause to view result
-    win.close()
 
 
 
@@ -28,7 +29,7 @@ def version1(d, dist, ang, x1, y1):
 from Queues import *
 def version2():
     q = Queue()
-    q.enqueue([8, 100, 90, 500,700])
+    q.enqueue([6, 100, 90, 500,700])
 
     while not q.empty():
        q0 = q.dequeue()
@@ -41,19 +42,15 @@ def version2():
        Line(Point(x1, y1), Point(found_point.getX(), found_point.getY())).draw(win)
 
        if d > 0:
-           q.enqueue((d-1, dist*0.8, ang-90, found_point.getX(), found_point.getY()))
-           q.enqueue((d - 1, dist*0.8, ang +90, found_point.getX(), found_point.getY()))
-    win.getMouse() # Pause to view result
-    win.close()
+           q.enqueue((d - 1, dist * 0.65, ang - 90, found_point.getX(), found_point.getY()))
+           q.enqueue((d - 1, dist / 1.5, ang + 30, found_point.getX(), found_point.getY()))
 
 choice = input("choose your version! ")
 if choice == "a":
-    win = GraphWin("My Fractal", 1000, 800)
-    win.setBackground(color="white")
-    version1(10, 100, 90, 500,700)
+    version1(6, 100, 90, 500,700)
 elif choice == "b":
-    win = GraphWin("My Fractal", 1000, 800)
-    win.setBackground(color="white")
     version2()
 
+win.getMouse() # Pause to view result
+win.close()
 
